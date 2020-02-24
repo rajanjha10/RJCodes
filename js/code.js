c1cd7f736ac309be02fce30097c29ff6bd6ee867
perfectionar <jhazzrajan@gmail.com>
2020-02-24 17:03:16 +0530

    Code fix

--- js/code.js
+++ js/code.js
@@ -7,6 +7,15 @@ function showCode(ele){
 	$('#show').toggle();
 }
 
+function loadDisqus(ele) { // DON'T EDIT BELOW THIS LINE
+	ele.classList.toggle("active");
+	$('#disqus_thread').toggle();
+	var d = document, s = d.createElement('script');
+	s.src = 'https://rj-codes.disqus.com/embed.js';
+	s.setAttribute('data-timestamp', +new Date());
+	(d.head || d.body).appendChild(s);
+};
+
 $(document).ready(function(){
 	var params = window.location.search.split('?')[1].split('&');
     var id = decodeURIComponent(params[0].split('=')[1]);
@@ -23,4 +32,10 @@ $(document).ready(function(){
 			$('#content').html(data);
 		}   
 	});
+
+	var disqus_config = function () {
+		this.page.url = window.location.search;  // Replace PAGE_URL with your page's canonical URL variable
+		this.page.identifier = id; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+	};
+
 });
